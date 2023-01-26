423bba5f0bbad4938e9371da3df3f1f07ecd579c
sohel284-coder <srana.bscse151284@gmail.com>
2023-01-26 14:40:59 +0600

    add two files in sum

---
+++ basic_program/sum/example1.py
@@ -0,0 +1,5 @@
+num1 = 15
+num2 = 25
+sum = num1 + num2
+sum = "Sum of {0} and {1} is {2}".format(num1, num2, sum)
+print(sum)
---
+++ basic_program/sum/example2.py
@@ -0,0 +1,7 @@
+number1 = input("First number: ")
+number2 = input("Second number: ")
+
+sum = float(number1) + float(number2)
+sum = "Sum of {0} and {1} is {2}".format(number1, number2, sum)
+
+print(sum)
